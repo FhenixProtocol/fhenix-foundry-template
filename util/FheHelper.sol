9cf13a48934e0e8ec50749b854844cad907b4933
Itzik Grossman <itzik@scrtlabs.com>
2024-08-13 19:06:14 +0300

    Move mockfheops from package to npm repo

--- util/FheHelper.sol
+++ util/FheHelper.sol
@@ -13,7 +13,7 @@ import {
 } from '@fhenixprotocol/contracts/FHE.sol';
 
 import { Test } from "forge-std/src/Test.sol";
-import { MockFheOps } from "./MockFheOps.sol";
+import { MockFheOps } from "@fhenixprotocol/contracts/utils/debug/MockFheOps.sol";
 
 contract FheEnabled is Test {
     function initializeFhe() public {
